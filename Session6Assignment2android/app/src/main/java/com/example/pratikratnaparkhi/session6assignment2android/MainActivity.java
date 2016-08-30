0c6933a8b141147ff2b8eaf40b5bc16bd50eefc2
gky_pro <pratikr557@gmail.com>
2016-08-30 21:06:26 +0530

    NA

---
+++ Session6Assignment2android/app/src/main/java/com/example/pratikratnaparkhi/session6assignment2android/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.pratikratnaparkhi.session6assignment2android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+
+
+    }
+}
